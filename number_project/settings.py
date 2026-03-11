b691b8837aa8e6a42a262ec01aca1301e70a58b2
milanmakwana1512 <milanmakwana1981@gmail.com>
2026-03-11 21:58:00 +0530

    fix routing

--- number_project/settings.py
+++ number_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*bj6)9^(whbr85fot!v=ebvk3f9_3qhg&f_dd5vp)0&#lw1b5w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['num2word-milan.onrender.com']
+ALLOWED_HOSTS = ['num2word-milan.onrender.com', '127.0.0.1']
 
 
 # Application definition
