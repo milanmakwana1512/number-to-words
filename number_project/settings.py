669e48a86e4cf159d6f1560e3c815141e074c5d1
milanmakwana1512 <milanmakwana1981@gmail.com>
2026-03-11 22:46:28 +0530

    fix allowed hosts

--- number_project/settings.py
+++ number_project/settings.py
@@ -25,9 +25,14 @@ SECRET_KEY = 'django-insecure-*bj6)9^(whbr85fot!v=ebvk3f9_3qhg&f_dd5vp)0&#lw1b5w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['num2word-Milan.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'num2word-milan-rnsd.onrender.com',
+    '127.0.0.1'
+]
 
-CSRF_TRUSTED_ORIGINS = ['https://num2word-Milan.onrender.com']
+CSRF_TRUSTED_ORIGINS = [
+    'https://num2word-milan-rnsd.onrender.com'
+]
 
 
 # Application definition
